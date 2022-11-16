e43de7189b3ae23be296fc091b2a2a6646e5452e
rlaalswo1222 <rlaalswo1222@naver.com>
2022-11-16 11:30:34 +0900

    feat: login page

--- src/gui/Login.java
+++ src/gui/Login.java
@@ -1,26 +1,98 @@
 package gui;
 
-import javax.swing.*;
 import api.user.UserDAO;
-
-
-public class Login {
-
-    private JButton logInButton;
-    private JTextField textField1;
-    private JPasswordField passwordField1;
-
+import java.awt.*;
+import javax.swing.*;
+/*
+ * Created by JFormDesigner on Wed Nov 16 09:47:05 KST 2022
+ */
 
 
 
+/**
+ * @author Minjae
+ */
+public class login extends JFrame {
+    public login() {
+        initComponents();
+    }
 
-    public static void main(String args[]) {
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        // Generated using JFormDesigner Evaluation license - Minjae
+        label1 = new JLabel();
+        label2 = new JLabel();
+        PWtext = new JPasswordField();
+        IDtext = new JTextField();
+        LoginButton = new JButton();
+        RememberCheck = new JCheckBox();
+        RegButton = new JButton();
 
-        JFrame frame = new JFrame("로그인");
+        //======== this ========
+        setTitle("LOGIN");
+        var contentPane = getContentPane();
+        contentPane.setLayout(null);
 
+        //---- label1 ----
+        label1.setText("ID");
+        contentPane.add(label1);
+        label1.setBounds(15, 15, 20, 17);
+
+        //---- label2 ----
+        label2.setText("PW");
+        contentPane.add(label2);
+        label2.setBounds(10, 45, 18, 20);
+        contentPane.add(PWtext);
+        PWtext.setBounds(35, 45, 195, 23);
+        contentPane.add(IDtext);
+        IDtext.setBounds(35, 15, 195, 23);
+
+        //---- LoginButton ----
+        LoginButton.setText("LOGIN");
+        contentPane.add(LoginButton);
+        LoginButton.setBounds(240, 15, 150, 55);
+
+        //---- RememberCheck ----
+        RememberCheck.setText("Remember me");
+        RememberCheck.setSelected(true);
+        contentPane.add(RememberCheck);
+        RememberCheck.setBounds(10, 75, 110, 21);
+
+        //---- RegButton ----
+        RegButton.setText("REGISTER");
+        contentPane.add(RegButton);
+        RegButton.setBounds(240, 75, 150, 23);
+
+        {
+            // compute preferred size
+            Dimension preferredSize = new Dimension();
+            for(int i = 0; i < contentPane.getComponentCount(); i++) {
+                Rectangle bounds = contentPane.getComponent(i).getBounds();
+                preferredSize.width = Math.max(bounds.x + bounds.width, preferredSize.width);
+                preferredSize.height = Math.max(bounds.y + bounds.height, preferredSize.height);
+            }
+            Insets insets = contentPane.getInsets();
+            preferredSize.width += insets.right;
+            preferredSize.height += insets.bottom;
+            contentPane.setMinimumSize(preferredSize);
+            contentPane.setPreferredSize(preferredSize);
+        }
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    public static void main(String[] args) {
+
+        com.formdev.flatlaf.intellijthemes.FlatArcOrangeIJTheme.setup();
+        login frame = new login();
+        frame.pack();
+        frame.setSize(415, 150);
+        frame.setVisible(true);
         UserDAO userDAO = new UserDAO();
         // addeventlistner
-        int result = userDAO.login(ID, Password);
+        /*
+        int result = userDAO.login(username, password);
         if (result == 1) {
             // 로그인 성공
         }
@@ -33,9 +105,16 @@ public class Login {
         else if (result == -2) {
             // 데이터베이스 오류
         }
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+    */
     }
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Evaluation license - Minjae
+    private JLabel label1;
+    private JLabel label2;
+    private JPasswordField PWtext;
+    private JTextField IDtext;
+    private JButton LoginButton;
+    private JCheckBox RememberCheck;
+    private JButton RegButton;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
