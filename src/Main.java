d5623a198ff0d0269b81dcbef0487771ee0d8ce5
ykkim010201@gmail.com <ykkim010201@gmail.com>
2022-12-05 12:58:06 +0900

    충돌 해결

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,5 @@
-import api.UserDAO;
-
-import java.util.Arrays;
-
 public class Main {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello world!");
-        UserDAO userDAO = new UserDAO();
-
-        String[] stringList = new String[8];
-        stringList = userDAO.userInformation(1);
-        System.out.println(Arrays.toString(stringList));
-
     }
 }
